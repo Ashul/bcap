dfaf4197962f2e2b6d83895a807714c8107ae350
Ashul Kumar <ashul.kumar@tothenew.com>
2020-09-10 09:29:35 +0530

    added https

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Routes from "./routes";
 import ErrorBoundary from '../src/components/ErrorBoundry';
+import HttpsRedirect from 'react-https-redirect';
 
 const App = (props) => (
+  <HttpsRedirect>
   <ErrorBoundary>
     <Routes/>
-  </ErrorBoundary>
+    </ErrorBoundary>
+    </HttpsRedirect>
 );
 
 

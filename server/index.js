c23ae128d208b9fc224c796b474a1a5de263abdd
Ashul Kumar <ashul.kumar@tothenew.com>
2020-09-29 18:49:44 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import serverRenderer from './middleware/renderer';
 
-const PORT = 3009;
+const PORT = process.env.PORT || 3009;
 const path = require('path');
 const app = express();
 const router = express.Router();
